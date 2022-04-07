19cac6420939988d7fb5a4c992660635ba15fd90
Rizky Dwi Saputra <55983435+rixkyduwi@users.noreply.github.com>
2022-04-07 13:48:40 +0700

    Update api_upload_image.py

--- api_upload_image.py
+++ api_upload_image.py
@@ -1,10 +1,20 @@
-from flask import Flask
-import jsonify
+from flask import Flask, jsonify
 app= Flask(__name__)
-app.config['']
-app.routes('/api/upload_image/' method=['POST'])
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+app.route('/api/upload_image/', methods=['POST'])
 def index():
-  imagename=
   return jsonify{
     "pesan":"gambar telah terupload"
   }
+
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0",debug = True, port=4000)
